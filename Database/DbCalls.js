db1d39c0131b4da2c81addefa7ece9de10c872d6
Winstun <arjan.vijn@student.nhlstenden.com>
2022-10-25 00:42:55 +0200

    Made connection to the database

--- Database/DbCalls.js
+++ Database/DbCalls.js
@@ -1,8 +1,10 @@
-// export function writeUserData(userId, name, email, imageUrl) {
-//   const db = getDatabase();
-//   set(ref(db, 'users/' + userId), {
-//     username: name,
-//     email: email,
-//     profile_picture : imageUrl
-//   });
-// }
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
+import app from "./DbConnect.js"
+
+export default function writeUserData(userId, name, email, imageUrl) {
+    const db = getDatabase(app);
+    set(ref(db, 'users/' + userId), {
+      username: name,
+      email: email
+    });
+  }
