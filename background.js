3fcd5ff77cabdb75fd20d922a797e24b80cb11ca
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2022-10-24 15:32:08 +0200

    Added location code

--- background.js
+++ background.js
@@ -1,46 +1,9 @@
 // background.js
-
-let color = '#3aa757';
-
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-
-  //getBookmarks()
+    // getCurrentUserLocation()
 });
 
-function getBookmarks()
+function getCurrentUserLocation()
 {
-    chrome.bookmarks.getTree()
-    .then(result => {
-      result.forEach(elements => {
-          elements["children"].forEach(element =>
-          { 
-            if(element.children != undefined)
-            {
-                console.log("========");
-                checkForChildrenInBookmarks(element)
-            }
-
-            //   console.log(element.children[1].title);
-            //   let folderChildren = element.children
-            //   folderChildren.forEach(directory =>
-            //   {
-            //       console.log(directory.title);
-            //   })
-              
-          })        
-      });
-    })
-}
-
-function checkForChildrenInBookmarks(dir)
-{
-    dir.children.forEach(directory =>{
-        console.log(directory.title);
-        if(directory.children != undefined)
-        {
-            checkForChildrenInBookmarks(directory)
-        }
-    })
+    getLocation()
 }
---
+++ geolocation/locationStuff.js
@@ -0,0 +1,31 @@
+//for now using button on options.html
+document.getElementById("getLocation").addEventListener('click', getLocation)
+
+function getLocation()
+{
+    navigator.geolocation.getCurrentPosition(position => {
+        const {longitude, latitude} = position.coords
+
+        const dbObj = {
+            latitude: latitude,
+            longitude: longitude
+        }
+
+        console.log(dbObj)
+
+        //TODO send to database
+    }, positionError => {
+        switch (positionError.code)
+        {
+            case GeolocationPositionError.POSITION_UNAVAILABLE:
+                console.log('no pos available')
+                break;
+            case GeolocationPositionError.PERMISSION_DENIED:
+                console.log('no permission')
+                break;
+            case GeolocationPositionError.TIMEOUT:
+                console.log('timed out')
+                break;
+        }
+    })
+}
