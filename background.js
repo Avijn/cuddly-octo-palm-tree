1b4e24c47f5afd67558af742a80b796aa47ffad5
winstun1 <arjan.witharen@gmail.com>
2022-10-24 15:38:41 +0200

    Tussentijdse push

--- background.js
+++ background.js
@@ -1,46 +1,16 @@
 // background.js
+import writeUserData from "./Database/DbConnect.js"
+//import  { writeUserData }  from "./Database/DbCalls.js"
+//import * as DbCalls from './Database/DbCalls'
+//require('./Database/DbCalls')
 
 let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
-
+  //writeUserData(1, "name", "email", "test");
+  writeUserData(1, "name", "email", "test");
+  console.log("test");
   //getBookmarks()
 });
-
-function getBookmarks()
-{
-    chrome.bookmarks.getTree()
-    .then(result => {
-      result.forEach(elements => {
-          elements["children"].forEach(element =>
-          { 
-            if(element.children != undefined)
-            {
-                console.log("========");
-                checkForChildrenInBookmarks(element)
-            }
-
-            //   console.log(element.children[1].title);
-            //   let folderChildren = element.children
-            //   folderChildren.forEach(directory =>
-            //   {
-            //       console.log(directory.title);
-            //   })
-              
-          })        
-      });
-    })
-}
-
-function checkForChildrenInBookmarks(dir)
-{
-    dir.children.forEach(directory =>{
-        console.log(directory.title);
-        if(directory.children != undefined)
-        {
-            checkForChildrenInBookmarks(directory)
-        }
-    })
-}
